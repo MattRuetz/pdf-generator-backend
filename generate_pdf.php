5a7ea9f33359c6b876cdf0dea65b0de4fea5b5c6
Matt Ruetz <matt.b.ruetz@gmail.com>
2024-11-23 15:46:37 -0800

    initial commit

---
+++ generate_pdf.php
@@ -0,0 +1,34 @@
+<?php
+require_once __DIR__ . '/vendor/autoload.php';
+
+header('Content-Type: application/json');
+header('Access-Control-Allow-Origin: https://invoicebutter.com');
+header('Access-Control-Allow-Methods: POST');
+header('Access-Control-Allow-Headers: Content-Type');
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $json = file_get_contents('php://input');
+    $data = json_decode($json, true);
+
+    if (!$data || !isset($data['html'])) {
+        http_response_code(400);
+        echo json_encode(['error' => 'Invalid input']);
+        exit;
+    }
+
+    try {
+        $mpdf = new \Mpdf\Mpdf();
+        $mpdf->WriteHTML($data['html']);
+        $pdfContent = $mpdf->Output('', 'S');
+
+        echo json_encode([
+            'pdf' => base64_encode($pdfContent)
+        ]);
+    } catch (Exception $e) {
+        http_response_code(500);
+        echo json_encode(['error' => $e->getMessage()]);
+    }
+} else {
+    http_response_code(405);
+    echo json_encode(['error' => 'Method not allowed']);
+}
